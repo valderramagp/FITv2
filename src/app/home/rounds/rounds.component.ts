bc29cf8a048ace2636ca956ebce8c8bff5180639
Gustavo Valderrama <gustavoavalderrama@gmail.com>
2018-04-18 00:14:35 -0500

    agrega funcionalidad para que nuevos usuarios empiecen desde 0

--- src/app/home/rounds/rounds.component.ts
+++ src/app/home/rounds/rounds.component.ts
@@ -7,6 +7,7 @@ import { UserService } from '../../core/user.service';
 import { FormBuilder, FormControl, Validators, FormGroup, FormArray } from '@angular/forms';
 import { Progress } from '../../shared/models/progress.model';
 import { LoaderService } from '../../core/loader.service';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
 	selector: 'app-rounds',
@@ -16,8 +17,9 @@ import { LoaderService } from '../../core/loader.service';
 export class RoundsComponent implements OnInit {
 	profile: Profile;
 	roundActivo: Round;
+	rounds: any[6] = new Array(6);
 	constructor(private db: AngularFirestore, private userService: UserService,
-		private fBuilder: FormBuilder, private loader: LoaderService) { }
+		private fBuilder: FormBuilder, private loader: LoaderService, private authService: AngularFireAuth) { }
 
 	ngOnInit() {
 		this.loader.start();
@@ -28,11 +30,24 @@ export class RoundsComponent implements OnInit {
 	}
 
 	getRounds() {
-		this.db.collection<Round>("rounds").valueChanges().subscribe(t => this.mergeRounds(t));
+		this.db.doc<Round>(`rounds/${this.calculateActiveRound()}`).valueChanges().subscribe(t => this.mergeRounds(t));
 	}
 
-	mergeRounds(rounds: Array<Round>) {
-		this.roundActivo = rounds.find(t => t.activo);
+	calculateActiveRound() {
+		let creationTime = Date.parse(this.authService.auth.currentUser.metadata.creationTime) - (1000 * 3600 * 4);
+		let fitStartDate = new Date(2018, 3, 8);
+		var userCreationDate = creationTime > fitStartDate.getTime() ? new Date(creationTime) : fitStartDate;
+		userCreationDate.setHours(0, 0, 0);
+		var currentDate = new Date();
+		currentDate.setHours(0, 0, 0);
+		let timeDiff = Math.abs(currentDate.getTime() - userCreationDate.getTime());
+		var daysDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
+		let activeRound = Math.ceil((daysDiff + userCreationDate.getDay()) / 7);
+		return activeRound;
+	}
+
+	mergeRounds(round: Round) {
+		this.roundActivo = round;
 		this.roundActivo.retos.forEach(reto => {
 			reto.form = this.buildForm(reto.nombre, reto.niveles[this.profile.nivel].dias);
 			reto.form.valueChanges.distinctUntilChanged().debounceTime(5000).subscribe((value) => this.guardarReto(reto.nombre, value));
