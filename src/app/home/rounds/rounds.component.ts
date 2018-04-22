2889d3713c8bb6d912d40b07882e12c5598735a4
Gustavo Valderrama <gustavoavalderrama@gmail.com>
2018-04-22 09:10:05 -0500

    round 3

--- src/app/home/rounds/rounds.component.ts
+++ src/app/home/rounds/rounds.component.ts
@@ -30,29 +30,42 @@ export class RoundsComponent implements OnInit {
 	}
 
 	getRounds() {
-		this.db.doc<Round>(`rounds/${this.calculateActiveRound()}`).valueChanges().subscribe(t => this.mergeRounds(t));
+		let activeRound = this.calculateActiveRound();
+		if (activeRound >= 1 && activeRound <= 6) {
+			this.db.doc<Round>(`rounds/${activeRound}`).valueChanges().subscribe(t => this.mergeRounds(t));
+		} else {
+			this.db.collection<Round>("rounds", ref => ref.where('activo', '==', true)).valueChanges().subscribe(t => this.mergeRounds(t[0]));
+		}
 	}
 
-	calculateActiveRound() {
-		let creationTime = Date.parse(this.authService.auth.currentUser.metadata.creationTime) - (1000 * 3600 * 4);
-		let fitStartDate = new Date(2018, 3, 8);
-		var userCreationDate = creationTime > fitStartDate.getTime() ? new Date(creationTime) : fitStartDate;
-		userCreationDate.setHours(0, 0, 0);
-		var currentDate = new Date();
-		currentDate.setHours(0, 0, 0);
-		let timeDiff = Math.abs(currentDate.getTime() - userCreationDate.getTime());
-		var daysDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
-		let activeRound = Math.ceil((daysDiff + userCreationDate.getDay()) / 7);
-		return activeRound;
+	calculateActiveRound(): number {
+		try {
+			let creationTime = Date.parse(this.authService.auth.currentUser.metadata.creationTime) - (1000 * 3600 * 4);
+			
+			let divideFlag = new Date(2018, 3, 22);
+			if (creationTime < divideFlag.getTime()) return 0;
+
+			let fitStartDate = new Date(2018, 3, 8);
+			var userCreationDate = creationTime > fitStartDate.getTime() ? new Date(creationTime) : fitStartDate;
+			userCreationDate.setHours(0, 0, 0);
+			var currentDate = new Date();
+			currentDate.setHours(0, 0, 0);
+			let timeDiff = Math.abs(currentDate.getTime() - userCreationDate.getTime());
+			var daysDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
+			let activeRound = Math.ceil((daysDiff + userCreationDate.getDay()) / 7);
+			return activeRound == 0 ? 1 : activeRound;
+		} catch (error) {
+			return 0;
+		}
 	}
 
 	mergeRounds(round: Round) {
-		this.roundActivo = round;
-		this.roundActivo.retos.forEach(reto => {
+		round.retos.forEach(reto => {
 			reto.form = this.buildForm(reto.nombre, reto.niveles[this.profile.nivel].dias);
-			reto.form.valueChanges.distinctUntilChanged().debounceTime(5000).subscribe((value) => this.guardarReto(reto.nombre, value));
-			let suscription = this.db.doc(`progress/${this.profile.usuario}/rounds/${this.roundActivo.descripcion.numero}/challenges/${reto.nombre}`).valueChanges().subscribe(value => { if (value) { reto.form.patchValue(value, { emitEvent: false }); } suscription.unsubscribe(); });
+			reto.form.valueChanges.distinctUntilChanged().debounceTime(2000).subscribe((value) => this.guardarReto(reto.nombre, value));
+			let suscription = this.db.doc(`progress/${this.profile.usuario}/rounds/${round.descripcion.numero}/challenges/${reto.nombre}`).valueChanges().subscribe(value => { if (value) { reto.form.patchValue(value, { emitEvent: false }); } suscription.unsubscribe(); });
 		});
+		this.roundActivo = round;
 		this.loader.stop();
 	}
 
